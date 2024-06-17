99c37b2de79c7003e5f298db9585ef3f0f01dc9e
Kishan Chauhan <chauhankishan446@gmail.com>
2024-06-17 19:32:33 +0530

    My commit

--- my-app.Dockerfile
+++ my-app.Dockerfile
@@ -1,7 +1,12 @@
-FROM node:16.20.1
-WORKDIR /app
+FROM node:14
+
+WORKDIR /usr/src/app
+
 COPY package*.json ./
 RUN npm install
+
 COPY . .
+
 EXPOSE 3000
-CMD ["npm","run","start"]
+CMD [ "node", "app.js" ]
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,72 +1,26 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAYORRFDZBFFL7RWXO"
-  secret_key = "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"
-}
-
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  region = "us-west-1"
+  access_key = 
+  secret_key = 
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+resource "aws_subnet" "subnet" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
-}
-
-data "aws_availability_zones" "available" {}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = element(aws_subnet.public[*].id, 0)
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = element(aws_subnet.public[*].id, 1)
-  route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "ecs" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -79,28 +33,54 @@ resource "aws_security_group" "ecs" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "hello-world-cluster"
 }
 
 resource "aws_ecs_task_definition" "hello_world" {
-  family                   = "hello-world-task"
-  requires_compatibilities = ["FARGATE"]
+  family                   = "hello-world"
   network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([{
-    name      = "hello-world"
-    image     = var.ecr_image
-    essential = true
+  container_definitions = jsonencode([
+    {
+      name  = "hello-world"
+      image = "${aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repository}:latest"
+      essential = true
 
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-    }]
-  }])
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
 }
 
 resource "aws_ecs_service" "hello_world" {
@@ -111,33 +91,14 @@ resource "aws_ecs_service" "hello_world" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = aws_subnet.public[*].id
+    subnets         = aws_subnet.subnet[*].id
     security_groups = [aws_security_group.ecs.id]
   }
 }
 
-resource "aws_lb" "app" {
-  name               = "hello-world-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs.id]
-  subnets            = aws_subnet.public[*].id
+data "aws_availability_zones" "available" {}
+
+resource "aws_ecr_repository" "hello_world_repo" {
+  name = "hello-world-repo"
 }
 
-resource "aws_lb_target_group" "app" {
-  name     = "hello-world-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_listener" "app" {
-  load_balancer_arn = aws_lb.app.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
